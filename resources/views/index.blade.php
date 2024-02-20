906ecb2ea5dfa2ea423ebf6814520047416c6759
David wanjohi <davidwagura83@gmail.com>
2024-02-20 12:23:40 +0300

    add view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,12 @@
-<form action=""></form>
+@extends('welcome')
+
+@section('content')
+
+<div class="text-center mt-32">
+    <button onclick="location.href='{{ route('movie.create') }}' " class="bg-blue-500 hover:bg-blue-700 mt-20 text-white h-10 font-bold px-4 border border-blue-700 rounded">
+       Add New Movie
+    </button>
+</div>
+
+
+@endsection
